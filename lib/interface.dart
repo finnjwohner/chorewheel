689040de79c08538bb5c3aa7ae46d377bbaae21d
Finn Wohner <71097713+frriNz@users.noreply.github.com>
2023-02-17 02:07:31 +0000

    Furthered nav bar
    - Added icons to nav bar
    - Added icon selecting when page switching

--- lib/interface.dart
+++ lib/interface.dart
@@ -1,11 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'homePage.dart';
 import 'chorePage.dart';
 import 'wishlistPage.dart';
 import 'leaderboardPage.dart';
 
-class Interface extends StatelessWidget {
+class Interface extends StatefulWidget {
   const Interface({super.key});
+
+  @override
+  State<Interface> createState() => _InterfaceState();
+}
+
+class _InterfaceState extends State<Interface> {
+  int pageIndex = 0;
 
   // This widget is the root of your application.
   @override
@@ -14,6 +22,9 @@ class Interface extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
+            onPageChanged: (value) => setState(() {
+              pageIndex = value;
+            }),
             children: const [
               HomePage(),
               ChorePage(),
@@ -24,7 +35,7 @@ class Interface extends StatelessWidget {
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
                 width: double.infinity,
                 height: 80,
                 decoration: BoxDecoration(
@@ -37,6 +48,18 @@ class Interface extends StatelessWidget {
                     offset: const Offset(0, 3),
                   ),],
                 ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(pageIndex == 0 ? CupertinoIcons.house_fill : CupertinoIcons.house, size: 40),
+                      Icon(pageIndex == 1 ? CupertinoIcons.hexagon_fill : CupertinoIcons.hexagon, size: 40),
+                      Icon(pageIndex == 2 ? CupertinoIcons.cube_box_fill : CupertinoIcons.cube_box, size: 40),
+                      Icon(pageIndex == 3 ? CupertinoIcons.heart_fill : CupertinoIcons.heart, size: 40),
+                    ],
+                  ),
+                )
               ),
           ),
         ],
